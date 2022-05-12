5a12aacfa6189a232c0ecfc21f09762b53265448
Vaibhav Singh <vaibhav_justin21@yahoo.in>
2022-05-12 12:52:10 +0530

    change shape and circle files

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+var shape =require('./shape');
 // declare class
+class Circle extends shape{
+    calculateArea() {
+    return 3.14*4*4;
+    }
 
-
+}
+module.exports=Circle;
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+    drawShape(){
+        console.log("Circle")
+    }
+    calculateArea(){
+        return 3.14*4*4;
+    }
+    color(){
+        this.color=red;
+    }
+}
 
+module.exports=Shape;
 // export class using module.exports
